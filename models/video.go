94e98dcbe3f6b62088e4ca7a15f53996a99582cd
mymoyu <1792292034@qq.com>
2022-05-20 21:02:53 +0800

    2022-5-20

--- models/video.go
+++ models/video.go
@@ -2,10 +2,12 @@ package models
 
 type Video struct {
 	Id            int64  `json:"id,omitempty" gorm:"primaryKey"`
-	Author        User   `json:"author"`
+	UserId        int64  `json:"-"`
+	Author        User   `json:"author", gorm:"foreignKey:userId"`
 	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
 	CoverUrl      string `json:"cover_url,omitempty"`
 	FavoriteCount int64  `json:"favorite_count,omitempty"`
 	CommentCount  int64  `json:"comment_count,omitempty"`
-	IsFavorite    bool   `json:"is_favorite,omitempty"`
+	IsFavorite    bool   `json:"is_favorite,omitempty"gorm:""`
+	Title         string `json:"title" gorm:"title"`
 }
