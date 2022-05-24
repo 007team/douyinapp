b84d9d324db15c522929896a695c9c8d771902b8
yeongbok77 <75071059+yeongbok77@users.noreply.github.com>
2022-05-24 23:40:00 +0800

    Update user.go

--- models/user.go
+++ models/user.go
@@ -3,9 +3,10 @@ package models
 
 import "time"
 
+
 type User struct {
 	Id            int64  `json:"id,omitempty"             gorm:"primaryKey; type:bigint(20) AUTO_INCREMENT;"`
-	Name          string `json:"name,omitempty"           gorm:"uniqueIndex:idx_name; type:varchar(64) UNIQUE not null" `
+	Name          string `json:"name,omitempty"           gorm:"uniqueIndex:idx_name; type:varchar(64) UNIQUE collate utf8mb4_general_ci not null" `
 	FollowCount   int64  `json:"follow_count,omitempty"   gorm:"column:follow_count; type:INT NOT NULL DEFAULT 0 "`
 	FollowerCount int64  `json:"follower_count,omitempty" gorm:"column:follower_count; type:INT NOT NULL DEFAULT 0"`
 	Password      string `json:"-"                        gorm:"column:password; type:varchar(200) NOT NULL"`
@@ -14,6 +15,7 @@ type User struct {
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
 }
+
 
 func (User) tableName() string {
 	return "users"
