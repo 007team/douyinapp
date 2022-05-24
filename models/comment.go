6bca1b0b8ecbc515a1bc32c63404abc2a1222798
yeongbok77 <75071059+yeongbok77@users.noreply.github.com>
2022-05-24 23:39:11 +0800

    Update comment.go

--- models/comment.go
+++ models/comment.go
@@ -6,7 +6,7 @@ type Comment struct {
 	Id        int64  `json:"id,omitempty"              gorm:"primaryKey; type:bigint(20) AUTO_INCREMENT"`
 	UserId    int64  `json:"-"                         gorm:"type:bigint(20) not null"`
 	Author    User   `json:"author"                    gorm:"foreignKey:UserId"`
-	Content   string `json:"content,omitempty"         gorm:"type:mediumtext NOT NULL"`
+	Content   string `json:"content,omitempty"         gorm:"type:mediumtext collate utf8mb4_general_ci NOT NULL"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
