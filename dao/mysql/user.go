91036dd75d9cc451e85ed9f29c754d96fc5f825f
yeongbok77 <75071059+yeongbok77@users.noreply.github.com>
2022-05-26 23:42:37 +0800

    Update user.go

--- dao/mysql/user.go
+++ dao/mysql/user.go
@@ -9,7 +9,7 @@ import (
 func UserInfo(user *models.User) (err error) {
 	// 根据 userid 查询数据库
 	if err = db.First(user, user.Id).Error; err != nil {
-		log.Fatalln("mysql.UserInfo 查询错误", err)
+		log.Println("mysql.UserInfo 查询错误", err)
 		return err
 	}
 
@@ -19,9 +19,9 @@ func UserInfo(user *models.User) (err error) {
 // CheckUserExist 查询用户是否存在
 func CheckUserExist(user *models.User) (err error) {
 	var count int64
-	err = db.Model(&models.User{}).Where("id = ?", user.Id).Count(&count).Error
+	err = db.Model(&models.User{}).Distinct("name").Where("name = ?", user.Name).Count(&count).Error
 	if err != nil {
-		log.Fatalln("mysql.CeckUserExist  用户数据查询失败", err)
+		log.Println("mysql.CeckUserExist  用户数据查询失败", err)
 		return err
 	}
 	if count > 0 {
@@ -34,7 +34,7 @@ func CheckUserExist(user *models.User) (err error) {
 // CreateNewUser 创建新的用户
 func CreateNewUser(user *models.User) (err error) {
 	if err = db.Select("Name", "Password", "Salt").Create(user).Error; err != nil {
-		log.Fatalln("mysql.CreateNewUser  创建新用户失败", err)
+		log.Println("mysql.CreateNewUser  创建新用户失败", err)
 		return err
 	}
 	return
@@ -43,3 +43,4 @@ func CreateNewUser(user *models.User) (err error) {
 func Login(user *models.User) (err error) {
 	return db.Where("name = ?", user.Name).First(&user).Error
 }
+
